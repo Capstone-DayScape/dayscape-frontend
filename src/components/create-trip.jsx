b100c811397da8673be0f810de8ed2cdd88d9e7d
AndyS120 <asalin12@gmail.com>
2024-10-06 20:59:43 -0400

    Added input extraction

--- src/components/create-trip.jsx
+++ src/components/create-trip.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
 export default function CreateTrip() {
+    // JSON to store data
+    const data = {
+        startingDate: "",
+        startingLocation: {
+            address: ""
+        }
+    };
+    const [dateObject, setDateObject] = React.useState(dayjs());
+    const [startingAddress, setStartingAddress] = React.useState("");
+
     const printToLog = () => {
-        console.log("Hello World");
+        data.startingLocation.address = startingAddress;
+        data.startingDate = dateObject.toJSON();
+        console.log(data);
     };
     return (
         <Box sx={{ width: 1 / 2, mx: "auto" }}>
@@ -13,20 +26,29 @@ export default function CreateTrip() {
                 <Typography variant="h2" sx={{ textAlign: "center" }} gutterBottom>
                     Create a new trip
                 </Typography>
-                <form id="createTripForm">
-                    <Stack direction="column" spacing={2}>
-                        <TextField label="Starting Location" name="startingLocation" required />
-                        <DatePicker label="Starting Date" name="startingDate" />
-                        {/*<Stack direction="row" spacing={2}>*/}
-                        {/*    <TextField label="Tags" name="tags" />*/}
-                        {/*    <Button variant="outlined">Add</Button>*/}
-                        {/*</Stack>*/}
-                        {/*<FormControlLabel control={<Checkbox />} label="Use Previous Stops" />*/}
-                        <Button variant="contained" onClick={printToLog}>
-                            Create Trip
-                        </Button>
-                    </Stack>
-                </form>
+                <Stack direction="column" spacing={2}>
+                    <TextField
+                        label="Starting Location"
+                        name="startingLocation"
+                        value={startingAddress}
+                        onChange={(event) => setStartingAddress(event.target.value)}
+                        required
+                    />
+                    <DatePicker
+                        label="Starting Date"
+                        name="startingDate"
+                        value={dateObject}
+                        onChange={(newDate) => setDateObject(newDate)}
+                    />
+                    {/*<Stack direction="row" spacing={2}>*/}
+                    {/*    <TextField label="Tags" name="tags" />*/}
+                    {/*    <Button variant="outlined">Add</Button>*/}
+                    {/*</Stack>*/}
+                    {/*<FormControlLabel control={<Checkbox />} label="Use Previous Stops" />*/}
+                    <Button variant="contained" onClick={printToLog}>
+                        Create Trip
+                    </Button>
+                </Stack>
             </LocalizationProvider>
         </Box>
     );
