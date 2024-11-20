6595aa682a6162963b9877f6ae2caa0107794fa3
AndyS120 <asalin12@gmail.com>
2024-11-19 23:55:11 -0500

    Added icon and changed layout (mobile)

--- src/components/node-info-dialog.js
+++ src/components/node-info-dialog.js
@@ -1,77 +1,44 @@
-import React from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, Box, TextField, Chip, Stack, FormControl, Rating, IconButton } from '@mui/material';
-import CallIcon from '@mui/icons-material/Call';
-import PublicIcon from '@mui/icons-material/Public';
-import SyncIcon from '@mui/icons-material/Sync';
+import CallIcon from "@mui/icons-material/Call";
+import PublicIcon from "@mui/icons-material/Public";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import SyncIcon from "@mui/icons-material/Sync";
+import {
+    Box,
+    Button,
+    Chip,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    FormControl,
+    IconButton,
+    Rating,
+    Stack,
+    TextField,
+    Typography
+} from "@mui/material";
+import React from "react";
 
-const NodeInfoDialog = ({ open, onClose, selectedNode, days, selectedDayIndex, handleHoursChange, handleMinutesChange, handleNotesChange, handleRegenerateNode }) => {
+const NodeInfoDialog = ({
+    open,
+    onClose,
+    selectedNode,
+    days,
+    selectedDayIndex,
+    handleHoursChange,
+    handleMinutesChange,
+    handleNotesChange,
+    handleRegenerateNode
+}) => {
     const handleRegenerateAndClose = () => {
         handleRegenerateNode();
         onClose();
     };
-    
+
     return (
         <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
             <DialogTitle>
-                <Box display="flex" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h6">
-                        {selectedNode?.name}
-                    </Typography>
-                    <Box display="flex" gap={3}>
-                        {selectedNode?.type && (
-                            <IconButton
-                                color="primary"
-                                onClick={handleRegenerateAndClose}
-                                sx={{
-                                    border: '1px solid',
-                                    borderRadius: '50%',
-                                    padding: '8px',
-                                    '&:hover': {
-                                        backgroundColor: 'rgba(0, 0, 0, 0.04)',
-                                    },
-                                }}
-                            >
-                                <SyncIcon />
-                            </IconButton>
-                        )}
-                        {selectedNode?.phone?.trim() && (
-                            <IconButton
-                                color="primary"
-                                href={`tel:${selectedNode.phone}`}
-                                sx={{
-                                    border: '1px solid',
-                                    borderRadius: '50%',
-                                    padding: '8px',
-                                    '&:hover': {
-                                        backgroundColor: 'rgba(0, 0, 0, 0.04)',
-                                    },
-                                }}
-                            >
-                                <CallIcon />
-                            </IconButton>
-                        )}
-                        {selectedNode?.website?.trim() && (
-                            <IconButton
-                                color="primary"
-                                href={selectedNode.website}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                sx={{
-                                    border: '1px solid',
-                                    borderRadius: '50%',
-                                    padding: '8px',
-                                    '&:hover': {
-                                        backgroundColor: 'rgba(0, 0, 0, 0.04)',
-                                    },
-                                }}
-                            >
-                                <PublicIcon />
-                            </IconButton>
-                        )}
-                    </Box>
-                </Box>
-            </DialogTitle>
-            <DialogContent>
+                <Typography variant="h6">{selectedNode?.name}</Typography>
                 <Typography variant="body1" gutterBottom>
                     {selectedNode?.info}
                 </Typography>
@@ -81,58 +48,122 @@ const NodeInfoDialog = ({ open, onClose, selectedNode, days, selectedDayIndex, h
                         <Rating value={selectedNode?.rating} readOnly precision={0.5} />
                     </Box>
                 )}
-                {selectedNode?.types && (
-                    <Stack direction="row" spacing={1} sx={{ flexWrap: "wrap" }} useFlexGap>
-                        <Typography variant="body1" sx={{ mt: 1 / 2 }}>
-                            Types:
-                        </Typography>
-                        {selectedNode.types.map((tag, index) => (
-                            <Chip
-                                variant="outlined"
-                                color={selectedNode.type === tag ? "primary" : "default"}
-                                label={tag}
-                                key={index}
-                            />
-                        ))}
-                    </Stack>
-                )}
-                {selectedNode?.label !== "1" && (
-                    <>
-                        <FormControl fullWidth variant="outlined" margin="normal">
-                            <Typography variant="body1">Duration:</Typography>
-                            <Box display="flex">
-                                <TextField
-                                    label="Hours"
-                                    type="number"
+                <Stack direction="row" justifyContent="space-evenly" sx={{ mt: 3, mb: 2 }}>
+                    {selectedNode?.type && (
+                        <>
+                            <IconButton
+                                color="error"
+                                onClick={() => {
+                                    // TODO: Implement delete node functionality
+                                    console.log("Delete Node!");
+                                }}
+                                sx={{ border: "1px solid", borderRadius: "50%" }}>
+                                <RemoveCircleIcon />
+                            </IconButton>
+                            <IconButton
+                                color="primary"
+                                onClick={handleRegenerateAndClose}
+                                sx={{
+                                    border: "1px solid",
+                                    borderRadius: "50%",
+                                    padding: "8px",
+                                    "&:hover": {
+                                        backgroundColor: "rgba(0, 0, 0, 0.04)"
+                                    }
+                                }}>
+                                <SyncIcon />
+                            </IconButton>
+                        </>
+                    )}
+                    {selectedNode?.phone?.trim() && (
+                        <IconButton
+                            color="primary"
+                            href={`tel:${selectedNode.phone}`}
+                            sx={{
+                                border: "1px solid",
+                                borderRadius: "50%",
+                                padding: "8px",
+                                "&:hover": {
+                                    backgroundColor: "rgba(0, 0, 0, 0.04)"
+                                }
+                            }}>
+                            <CallIcon />
+                        </IconButton>
+                    )}
+                    {selectedNode?.website?.trim() && (
+                        <IconButton
+                            color="primary"
+                            href={selectedNode.website}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            sx={{
+                                border: "1px solid",
+                                borderRadius: "50%",
+                                padding: "8px",
+                                "&:hover": {
+                                    backgroundColor: "rgba(0, 0, 0, 0.04)"
+                                }
+                            }}>
+                            <PublicIcon />
+                        </IconButton>
+                    )}
+                </Stack>
+            </DialogTitle>
+            <DialogContent>
+                <Stack direction="column" spacing={2}>
+                    {selectedNode?.types && (
+                        <Stack direction="row" spacing={1} sx={{ flexWrap: "wrap" }} useFlexGap>
+                            <Typography variant="body1" sx={{ mt: 1 / 2 }}>
+                                Types:
+                            </Typography>
+                            {selectedNode.types.map((tag, index) => (
+                                <Chip
                                     variant="outlined"
-                                    margin="normal"
-                                    value={days[selectedDayIndex].durations[selectedNode?.name]?.hours}
-                                    onChange={handleHoursChange}
-                                    style={{ marginRight: "10px" }}
-                                    slotProps={{ htmlInput: { min: 0 } }}
+                                    color={selectedNode.type === tag ? "primary" : "default"}
+                                    label={tag}
+                                    key={index}
                                 />
-                                <TextField
-                                    label="Minutes"
-                                    type="number"
-                                    variant="outlined"
-                                    margin="normal"
-                                    value={days[selectedDayIndex].durations[selectedNode?.name]?.minutes}
-                                    onChange={handleMinutesChange}
-                                    slotProps={{ htmlInput: { min: 0 } }}
-                                />
-                            </Box>
-                        </FormControl>
-                    </>
-                )}
-                <TextField
-                    label="Enter Notes"
-                    multiline
-                    rows={4}
-                    variant="outlined"
-                    fullWidth
-                    value={days[selectedDayIndex].notes[selectedNode?.name] || ""}
-                    onChange={handleNotesChange}
-                />
+                            ))}
+                        </Stack>
+                    )}
+                    {selectedNode?.label !== "1" && (
+                        <>
+                            <FormControl fullWidth variant="outlined" margin="normal">
+                                <Typography variant="body1">Duration:</Typography>
+                                <Box display="flex">
+                                    <TextField
+                                        label="Hours"
+                                        type="number"
+                                        variant="outlined"
+                                        margin="normal"
+                                        value={days[selectedDayIndex].durations[selectedNode?.name]?.hours}
+                                        onChange={handleHoursChange}
+                                        style={{ marginRight: "10px" }}
+                                        slotProps={{ htmlInput: { min: 0 } }}
+                                    />
+                                    <TextField
+                                        label="Minutes"
+                                        type="number"
+                                        variant="outlined"
+                                        margin="normal"
+                                        value={days[selectedDayIndex].durations[selectedNode?.name]?.minutes}
+                                        onChange={handleMinutesChange}
+                                        slotProps={{ htmlInput: { min: 0 } }}
+                                    />
+                                </Box>
+                            </FormControl>
+                        </>
+                    )}
+                    <TextField
+                        label="Enter Notes"
+                        multiline
+                        rows={4}
+                        variant="outlined"
+                        fullWidth
+                        value={days[selectedDayIndex].notes[selectedNode?.name] || ""}
+                        onChange={handleNotesChange}
+                    />
+                </Stack>
             </DialogContent>
             <DialogActions>
                 <Button onClick={onClose}>Close</Button>
