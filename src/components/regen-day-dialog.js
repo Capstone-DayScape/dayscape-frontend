a149a30bf533931068eb6cf395425132d5134a8f
AndyS120 <asalin12@gmail.com>
2024-11-27 23:00:15 -0500

    Removed formatter errors

--- src/components/regen-day-dialog.js
+++ src/components/regen-day-dialog.js
@@ -3,16 +3,14 @@ import { Button, Dialog, DialogActions, DialogContent, DialogContentText, Dialog
 export const RegenDayDialog = ({ open, onClose, onAccept }) => {
     return (
         <Dialog open={open}>
-            {" "}
-            <DialogTitle>Do you want to regenerate this day?</DialogTitle>{" "}
+            <DialogTitle>Do you want to regenerate this day?</DialogTitle>
             <DialogContent>
                 <DialogContentText>This will replace all node for this day with new ones.</DialogContentText>
-            </DialogContent>{" "}
+            </DialogContent>
             <DialogActions>
-                {" "}
                 <Button onClick={onClose}>Cancel</Button>
-                <Button onClick={onAccept}>Yes</Button>{" "}
-            </DialogActions>{" "}
+                <Button onClick={onAccept}>Yes</Button>
+            </DialogActions>
         </Dialog>
     );
 };
